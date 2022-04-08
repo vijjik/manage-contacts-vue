9842f732b29467449c2f889085bec1e9dd9e2178
vg <vijji.konuru@gmail.com>
2022-04-08 19:49:41 +0530

    intializing vue setup - vite

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Register from "./components/SignUp.vue";
+</script>
+
+<template>
+	<header>
+		<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+		<div class="wrapper">
+			<h1>Empolyee Contacts Managing Tool</h1>1
+		</div>
+	</header>
+
+	<main>
+		<Register />
+	</main>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
